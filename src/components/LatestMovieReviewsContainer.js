c184e8c10626d761baff3fde6a6d3ee3f18957ba
Taylor <stein0209@gmail.com>
2019-09-22 15:06:18 -0600

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -2,8 +2,36 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
+const NYT_API_KEY = 'rRrv2lve4nKtva81lMMCAAKUhJvzc8ve';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
-// Code LatestMovieReviewsContainer Here
+export default class LatestMovieReviewsContainer extends Component {
+    state = {
+        latestMovieReviews: []
+    }
+
+    componentDidMount(){
+        fetch(`${URL}`)
+            .then(response => response.json())
+            .then(res => this.setState({latestMovieReviews: res.results}))
+    }
+
+    renderLatestMovieCards = () => {
+        return this.state.latestMovieReviews.map((movie, index) => {
+            return <MovieReviews className='review' key={index} movie={movie} />
+        })
+    }
+
+    render() {
+        return (
+            <div className='latest-movie-reviews'>
+                <h1>Latest Movie Reviews</h1>
+                <div className='review-list'>
+                    {this.renderLatestMovieCards()}
+                </div>
+            </div>
+        )
+    }
+}
+
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -2,8 +2,36 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
-const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
+const NYT_API_KEY = 'rRrv2lve4nKtva81lMMCAAKUhJvzc8ve';
+const URL = `https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=`
             + `api-key=${NYT_API_KEY}`;
 
-// Code SearchableMovieReviewsContainer Here
+export default class SearchableMovieReviewsContainer extends Component {
+    state = {
+        searchedMovieReviews: [],
+        query: 'lebowski'
+    }
+
+    componentDidMount(){
+        fetch(`${URL}`)
+            .then(response => response.json())
+            .then(res => this.setState({searchedMovieReviews: res.results}))
+    }
+
+    renderSearchedMovieCards = () => {
+        return this.state.searchedMovieReviews.map((movie, index) => {
+            return <MovieReviews className='review' key={index} movie={movie} />
+        })
+    }
+
+    render() {
+        return (
+            <div className='searchable-movie-reviews'>
+                <div className='review-list'>
+                    {/* {this.renderSearchedMovieCards()} */}
+                </div>
+            </div>
+        )
+    }
+}
+
