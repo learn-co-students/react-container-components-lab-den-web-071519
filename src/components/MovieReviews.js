d691114053dcbb9b6ec802da4886092942180d79
SwanHub <jdprince555@gmail.com>
2019-09-23 21:19:07 -0600

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,16 @@
-// Code MovieReviews Here
+import React from 'react'
+
+export default function MovieReviews(props) {
+    
+    const renderMovies = () => {
+        return props.reviews.map((review) => {
+            return <li>{review.display_title}</li>
+        })
+    }
+
+    return (
+        <div className="review-list">
+            {renderMovies()}
+        </div>
+    )
+}
